fa594dafb3534a8e21fab15b3a9070de321dd14c
Vidit Shah <vidit0210@protonmail.com>
2025-01-03 13:43:58 +0530

    exporting client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { useS3Image } from "./hooks/useS3Image";
 export { S3Image } from "./components/S3Image";
+export { createS3Client } from "./hooks/useS3Image";
 export type { S3ImageProps } from "./components/S3Image";
